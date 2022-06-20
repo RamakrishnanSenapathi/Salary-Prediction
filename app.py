b9c9604aff5de0ed7a082104b579dbfe3b3fec7a
RAMAKRISHNAN SENAPATHI <93332089+RamakrishnanSenapathi@users.noreply.github.com>
2022-06-20 15:24:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template,request
+import numpy as np
+import pickle
+
+#creating constructor
+app=Flask(__name__, template_folder='templates', static_folder='static')
+model=pickle.load(open('model\LR.pkl', 'rb'))
+#print(model)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''v1 = request.form['YearsExperience']'''
+    features=[int(x) for x in request.form.values()]
+    final_feature=[np.array(features)]
+    pred = model.predict(final_feature)
+    out=pred
+
+    return render_template('home.html', prediction=out)
+if __name__ == '__main__':
+    app.run(debug=True)     
